b0b57bf3d0c1228e6a4407c4626b21b35590acf9
Tubalevião <alvaro.prata.filho@gmail.com>
2020-09-22 21:12:49 -0400

    eureka now working

--- student/src/main/java/am/i/student/StudentApplication.java
+++ student/src/main/java/am/i/student/StudentApplication.java
@@ -3,6 +3,8 @@ package am.i.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,6 +13,11 @@ public class StudentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 		System.out.println("Done!");
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
