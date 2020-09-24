7c23ce514705b081e2ca6db62f84d792be18cf4c
Tubalevião <alvaro.prata.filho@gmail.com>
2020-09-24 12:24:49 -0400

    gatway code

---
+++ gateway/src/main/java/am/i/gateway/service/ServiceGatway.java
@@ -0,0 +1,35 @@
+package am.i.gateway.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.stereotype.Service;
+import am.i.databaseBuilder.Course;
+
+@Service
+public class ServiceGatway implements IService{
+	
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private EurekaClient eurekaClient;
+	@Value("${country-service}")
+	private String courseService;
+
+	@Override
+	public List<Course> getCourses() {
+		return restTemplate.getForObject(myEurekaLookup(courseService) + "/courses", List.class);
+	}
+	
+	private String myEurekaLookup(String serviceName) {
+		InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka(serviceName, false);
+		return instanceInfo.getHomePageUrl();
+	}
+
+}
