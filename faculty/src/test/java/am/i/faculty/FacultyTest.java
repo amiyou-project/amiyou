8c89f864ec6609a2c281014127ef1dddbc1c5403
sammy <samueltesfai41@gmail.com>
2020-09-24 10:37:38 -0500

    update with a test

--- faculty/src/test/java/am/i/faculty/FacultyTest.java
+++ faculty/src/test/java/am/i/faculty/FacultyTest.java
@@ -2,24 +2,16 @@
 package am.i.faculty;
 import static org.mockito.Mockito.when;
 
-import java.util.Date;
-import java.util.Optional;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-
 import am.i.faculty.domain.Attendance;
-import am.i.faculty.domain.Course;
 import am.i.faculty.repository.AttendanceRepository;
 import am.i.faculty.service.AttendanceService;
 
@@ -35,9 +27,21 @@ public class FacultyTest {
    
    @Test
    public void getAttendanceTest() {
-      when(attendanceRepository.findById(0).get().getDescription())
-      .thenReturn("remote");
+//      when(attendanceService.getAttendanceById(0).getDescription())
+//      .thenReturn("remote");
       String testName = attendanceService.getAttendanceById(0).getDescription();
+      System.out.println(testName + "8888888888888888888888888888888888888888888888888888888888888888888888");
       Assert.assertEquals("remote", testName);
    }
+
+//	@Test
+//	public void getInstructors() {
+//		when(attendanceRepository.findAll())
+//				.thenReturn(
+//						java.util.stream.Stream
+//								.of(new Attendance(1, "Dereje", "Mock", 1, "Mock Tester", 3),
+//										new Attendance(2, "Najeeb", "Najeeb", 5, "EArchitect", 10))
+//								.collect(Collectors.toList()));
+//		assertEquals(2, attendanceService.getAllInstructors().size());
+//	}
 }
