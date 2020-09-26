372e0bebff8c12695fa5a5f04d84f189becde93e
Tubalevião <alvaro.prata.filho@gmail.com>
2020-09-26 12:14:29 -0400

    merging tmservice changes

--- tm/src/main/java/am/i/tm/tmservice/tmServiceImpl.java
+++ tm/src/main/java/am/i/tm/tmservice/tmServiceImpl.java
@@ -1,8 +1,13 @@
 package am.i.tm.tmservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import DTO.Student;
 import am.i.tm.tmRepository.ApptRepository;
 import am.i.tm.tmRepository.InstructorRepository;
 import am.i.tm.tmRepository.tmRepository;
@@ -12,7 +17,7 @@ import am.i.tm.tmdomain.TMInstructor;
 
 @Service
 public class tmServiceImpl implements tmService {
-
+	 
 	@Autowired
 	private tmRepository tmrepository;
 	@Autowired
@@ -35,7 +40,6 @@ public class tmServiceImpl implements tmService {
 
 	@Override
 	public TMInstructor saveOrUpdateInstructor(TMInstructor instructor) {
-		// instructor.addStudent(2);
 		instructorRepository.save(instructor);
 		return instructor;
 	}
@@ -45,23 +49,18 @@ public class tmServiceImpl implements tmService {
 		TMInstructor instructor = getInstructorById(inst_id);
 		instructor.addStudent(stud_id);
 		return saveOrUpdateInstructor(instructor);
-		// return //instructor;
 
 	}
 
 	public TMInstructor getInstructorById(int id) {
-		if (instructorRepository.findById(id).isPresent()) {  
+		if (instructorRepository.findById(id).isPresent()) {
 			return instructorRepository.findById(id).get();
 		}
 		return null;
 	}
-	
-	  @Override
-	  public TMInstructor getInstructorByName(String lname) {
-		  return instructorRepository.findBylname(lname); 
-		  }
-	 
-	
-	
-	
+
+	@Override
+	public TMInstructor getInstructorByName(String lname) {
+		return instructorRepository.findBylname(lname);
+	}
 }
